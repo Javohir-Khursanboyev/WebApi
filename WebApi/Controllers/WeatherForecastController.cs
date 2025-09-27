63349330a5965a0d016a8568e6b2936dc6586d45
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-09-28 01:43:01 +0500

    changed

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -29,12 +29,5 @@ namespace WebApi.Controllers
             })
             .ToArray();
         }
-
-        //[HttpGet("error")]
-        //public IActionResult GetError()
-        //{
-        //    throw new Exception("This is a test exception");
-        //}
     }
-
 }
